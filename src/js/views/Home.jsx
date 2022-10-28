e7c58df445fda6cc9bae8f2ca6e6b7eccda033ca
MireyaCR <mireya.mcr@gmail.com>
2022-10-28 22:54:24 +0000

    starwarsreact

--- src/js/views/Home.jsx
+++ src/js/views/Home.jsx
@@ -2,15 +2,46 @@ import React from "react";
 
 const Home=()=> {
     return(
-      
-            <div className="card" style={{width: "18rem"}}>
-               <img src="..." class="card-img-top" alt="..."/>
-               <div className="card-body">
-                    <h5 className="card-title">El titulo</h5>
-                    <p className="card-text">La descripcion.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere te lleva a la vista de la tarjeta con mas información</a>
-                </div>
+        <div className="container-fluid">
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                <div className="col"></div>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img src="..." class="card-img-top" alt="..."/>
+                        <div className="card-body">
+                                <h5 className="card-title">El titulo</h5>
+                                <p className="card-text">La descripcion.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere te lleva a la vista de la tarjeta con mas información</a>
+                        </div>
+                    </div>
             </div>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                <div className="col"></div>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img src="..." class="card-img-top" alt="..."/>
+                        <div className="card-body">
+                                <h5 className="card-title">El titulo</h5>
+                                <p className="card-text">La descripcion.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere te lleva a la vista de la tarjeta con mas información</a>
+                        </div>
+                    </div>
+            </div>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                <div className="col"></div>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img src="..." class="card-img-top" alt="..."/>
+                        <div className="card-body">
+                                <h5 className="card-title">El titulo</h5>
+                                <p className="card-text">La descripcion.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere te lleva a la vista de la tarjeta con mas información</a>
+                        </div>
+                    </div>
+            </div>
+
+
+
+        </div>
+  
+   
     )
 }
 export default Home;
